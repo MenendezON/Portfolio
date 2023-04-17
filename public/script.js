b66b2695324a3fc1ef0ed03fdb41bc955df1998a
MenendezON <menendezon@gmail.com>
2023-04-17 13:40:18 +0000

    Update public folder contents

--- public/script.js
+++ public/script.js
@@ -1,24 +1,23 @@
-"use strict"
-let menu = document.querySelector('.hamb');
-let title = document.querySelector('h1');
-let intro = document.querySelector('p');
-let nav = document.querySelector('.nav');
-let btnclose = document.querySelector('.close');
-let item = document.querySelector('.menu');
-menu.addEventListener("click", function(){
-    title.style.display = "none";
-    intro.style.display = "none";
-    nav.style.display = "block";
+const menu = document.querySelector('.hamb');
+const title = document.querySelector('h1');
+const intro = document.querySelector('p');
+const nav = document.querySelector('.nav');
+const btnclose = document.querySelector('.close');
+const item = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+  title.style.display = 'none';
+  intro.style.display = 'none';
+  nav.style.display = 'block';
 });
-btnclose.addEventListener('click', function(){
-    document.getElementById('side-menu').checked = false;
-    nav.style.display = "none";
-    title.style.display = "block";
-    intro.style.display = "block";
+btnclose.addEventListener('click', () => {
+  document.getElementById('side-menu').checked = false;
+  nav.style.display = 'none';
+  title.style.display = 'block';
+  intro.style.display = 'block';
 });
-item.addEventListener('click', function(){
-    document.getElementById('side-menu').checked = false;
-    nav.style.display = "none";
-    title.style.display = "block";
-    intro.style.display = "block";
+item.addEventListener('click', () => {
+  document.getElementById('side-menu').checked = false;
+  nav.style.display = 'none';
+  title.style.display = 'block';
+  intro.style.display = 'block';
 });
