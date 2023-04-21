197c0392660acad4eaa0fa3c6d3776aeabdaa596
MenendezON <menendezon@gmail.com>
2023-04-21 08:17:29 +0000

    Update the popup-feature

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// Activity 1 : Manage de menu
 const menu = document.querySelector('.hamb');
 const title = document.querySelector('h1');
 const intro = document.querySelector('p');
@@ -27,43 +28,19 @@ item.addEventListener('click', () => {
   display(intro, 'block');
 });
 
-// close the modal
-const closeBtn = document.querySelectorAll('.closeBtn');
-closeBtn.forEach((clsBtn) => {
-  clsBtn.addEventListener('click', (event) => {
-    const cl = event.target.parentNode;
-    cl.style.display = 'none';
-    cl.parentNode.setAttribute('class', 'project achievement p1');
-
-    document.getElementById('btn 1').remove();
-    document.getElementById('btn 2').remove();
-    document.getElementById('btn').style.display = 'block';
-  });
-});
-// generate a button
-function createBtn(type, content, lstClass) {
-  const btn = document.createElement('button');
+// Activity 2 : Lauch the modal
+function createBtn(type, content, lstClass, link) {
+  const btn = document.createElement('a');
   btn.innerHTML = content;
   btn.setAttribute('class', lstClass);
   btn.setAttribute('id', lstClass);
+  btn.setAttribute('href', link);
   return type.appendChild(btn);
 }
-// open the modal
-const viewpjt = document.querySelectorAll('.btn');
-viewpjt.forEach((btns) => {
-  btns.addEventListener('click', (event) => {
-    const ach = event.target.parentNode;
-    // console.log(event);
-    ach.parentNode.setAttribute('class', 'project popup');
-    document.querySelector('.closeBtn').style.display = 'block';
 
-    btns.style.display = 'none';
 
-    createBtn(ach, "See live <img src='./public/images/goonline.jpeg' alt=' '>", 'btn 1');
-    createBtn(ach, "Hello Button <img src='./public/images/github.png' alt=' '>", 'btn 2');
-  });
-});
 
+// Activity 3 : Validation form
 function validateForm() {
   const emailField = document.getElementById('email');
   const email = emailField.value;
@@ -79,3 +56,5 @@ function validateForm() {
 }
 
 document.getElementsByClassName('btnForm')[0].addEventListener('submit', validateForm);
+
+// Activity 4 : Local storage
